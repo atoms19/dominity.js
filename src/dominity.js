21c2bc89ce5392b0b134d42bc48be1c0271cbdba
atoms19 <74504022+atoms19@users.noreply.github.com>
2024-08-31 07:19:13 +0530

    added js doc commenst big revamp

--- src/dominity.js
+++ src/dominity.js
@@ -495,7 +495,19 @@ var startBatch = function() {
   var derived = computed;
   var DominityReactive = Signal;
   //------------------------------------------💓💓💓💓💓💓💓 thanks for scrolling ------------------------------------------------
+
+/**
+ * 
+ * a wrapper around html elements that enables dominity to function 
+ * 
+ */
   class DominityElement {
+
+    /**
+     * 
+     * @param {string|HTMLElement} qry 
+     * @returns {this}
+     */
     constructor(qry) {
       if (typeof qry == "string") {
         this.elem = document.querySelector(qry);
@@ -507,6 +519,11 @@ var startBatch = function() {
         return;
       }
     }
+    /**
+     * sets inner HTML
+     * @param {null |string} val - html markup as string ,returns elements innerHTML if left empty 
+     * @returns {this}
+     */
     html(val) {
       if (val == null) {
         return this.elem.innerHTML;
@@ -521,6 +538,12 @@ var startBatch = function() {
         return this;
       }
     }
+    /**
+     * used to apply css styling to elements
+     * @param {object|string} prp -pass in an object containing key value pairs of properties or a single string propertyname to be set or get
+     * @param {undefined|stirng} val -if left empty the single string property computed value is returned else its set to the value passed in
+     * @returns {this}
+     */
     css(prp, val = undefined) {
       if (typeof prp == "string") {
         if (val == undefined) {
@@ -539,6 +562,12 @@ var startBatch = function() {
         return this;
       }
     }
+    /**
+     * used to add attributes to an element
+     * @param {object|string} prp -pass in an object containing key value pairs of attributes or a single string attributename to be set or get
+     * @param {undefined|stirng} val -if left empty the single string atribute value is returned else its set to the value passed in
+     * @returns {this}
+     */
     attr(prp, val = undefined) {
       if (typeof prp == "string") {
         if (val == undefined) {
@@ -567,15 +596,28 @@ var startBatch = function() {
         return this;
       }
     }
+    /**
+     * used to attach events to an element
+     * @param {string} e -event name
+     * @param {Function} cb- function to be called  
+     * @param {boolean} bub -event capture false by default
+     * @returns {this}
+     */
     on(e, cb, bub = false) {
       this.elem.addEventListener(e, cb, bub);
       return this;
     }
+    /** removes the element from the dom */
     remove() {
       this.elem.remove();
       return this;
     }
     //css based rerenderer 
+    /**
+     * shows and hides the element based on truthineess of passed in state or return value of function that utilises a state
+     * @param {DominityReactive|Function} expression -reactive state whos truth value affects the rendering  or function
+     * @returns {this}
+     */
     showIf(expression) {
       let storedDisplay = this.css("display") != "none" ? this.css("display") : "block";
       if (typeof expression == "function") {
@@ -595,24 +637,41 @@ var startBatch = function() {
       return this;
     }
   //loop
+  /**
+   * iterates over a list and renders a component for each item
+   * @param {DominityReactive} list-reactive state iterable who will be iterated 
+   * @param {Function} callback the function must return a component or the function must be a component (this function recives (item , count,parent elements reference ))
+   * @returns {this}
+   */
     forEvery(list, callback) {
       let elemS = this;
       if (list instanceof DominityReactive) {
         effect2(() => {
           elemS.elem.innerHTML = "";
           list.value.forEach((item, count) => {
-            callback(item, elemS, count).addTo(this);
+            callback(item, count,elemS).addTo(this);
           });
         });
         return this;
       }
-      console.error("DominityError: list item for ._elFor has to be a reactive object made with rst() and iterable");
+      console.error("DominityError: list item for .forEvery has to be a reactive object made with state() and iterable");
       return this;
     }
+    /**
+     * used to run a function with reference to element 
+     * @param {Function} func a function that recives js refernce and dominity reference as first and second parameters 
+     * @returns {this} 
+     */
     withRef(func){
       func(this.elem,this)
       return this
     }
+    /**
+     * used to store the elements javascript/DOminity refernce to a state
+     * @param {DominityReactive} ref 
+     * @param {boolean} org if true js reference is returned else its dominity reference
+     * @returns 
+     */
     giveRef(ref,org=true){
       if(ref instanceof DominityReactive){
         ref.value=org?this.elem:this
@@ -621,7 +680,12 @@ var startBatch = function() {
       }
       return this
     }
-  
+  /**
+   * used to model an input's value to a state (2 way databinding)
+   * @param {DominityReactive} target state to be modeled to  
+   * @param {{debounce?:number,throttle:?number}} options -options are used to set optional throttle and debounce values incase 
+   * @returns 
+   */
     model(target, options) {
       let attr = "value"; //todo radio groups doesnt work 
       if (this.attr("type") == "checkbox") {
@@ -687,6 +751,14 @@ var startBatch = function() {
       }
       return this;
     }
+    /**
+     * 
+     * @param {object} props properties to be animated as an object and their final values 
+     * @param {number} duration duration in milliseconds
+     * @param {string} easing easing function 
+     * @param {Function} callback -call back that runs after the animation ends 
+     * @returns {this}
+     */
     animate(props, duration, easing = "linear", callback = undefined) {
       let priorkeyframes = {};
       Object.keys(props).forEach((prop) => {
@@ -712,12 +784,47 @@ var startBatch = function() {
     }
     
   }
-  
+  /**sets up a client side router to handle page views */
   export class DominityRouter {
     constructor() {
       this.routeMap = {};
       this.defaultPath = undefined;
     }
+    /**
+     * sets the routes
+     * @param {object} routeMap
+     * 
+     * @example
+     * 
+     * ### using component directly
+     * ```
+     * {
+     * "/home":{
+     *    component:home()
+     * }
+     * ,"about":{...}
+     * 
+     * }
+     * ```
+     * 
+     * ### using component as function 
+     * ```
+     * 
+     * {
+     *  "/home":{
+     *     getComponent:home,
+     *   onLoad(){
+     *      console.log('home')
+     *  },
+     *  layout(component){
+     *    return someLayout(component)
+     *  }
+     * }
+     * 
+     * }
+     * ```
+     *  
+     */
     setRoutes(routeMap = {}){
       
       Object.keys(routeMap).forEach((key) => {
@@ -760,8 +867,16 @@ var startBatch = function() {
         this.routeMap[key] = routeobj;
       });
     }
+    /**
+     * sets root of router , ie the element wheer router elements will be rendered 
+     * @param {HTMLElement|DominityElement} root -element to be passed in as root of router
+     */
     start(root) {
+      if(root instanceof DominityElement){
+        this.root=root.elem
+      }else{
       this.root=root
+      }
       addEventListener("popstate", () => {
         setTimeout(() => {
           this.assignRoute();
@@ -778,6 +893,7 @@ var startBatch = function() {
       });
       this.assignRoute();
     }
+
     async assignRoute() {
       Object.keys(this.routeMap).forEach((route) => {
         let routeData = this.routeMap[route];
@@ -788,19 +904,36 @@ var startBatch = function() {
         }
       });
     }
+    /**
+     * routes to a specific new route 
+     * @param {string} route -
+     */
     routeTo(route) {
       history.pushState(null, "", route);
       this.assignRoute();
     
     }
+    /**
+     * replaces the current route with new route
+     * @param {string} route 
+     */
     replaceRoute(route) {
       history.replaceState(null, "", route);
       this.assignRoute();
     }
+    /**
+     * reruns the route handler of current route
+     */
     revalidateRoute() {
       history.go(0);
       this.assignRoute();
     }
+    /**
+     * a replacement for anchor tags that works with the router 
+     * @param {{href:string,replace:boolean,...attrs:any}} param0 - replace if true route is replaced instead of new redirect
+     * @param  {...any} args -remaining components
+     * @returns 
+     */
     Link({href,replace,...attrs},...args) {
       return el("a",{...attrs}, ...args).on("click", (e) => {
         e.preventDefault();
@@ -812,11 +945,32 @@ var startBatch = function() {
         }
       });
     }
+    /**gets all search queries as object */
     get queries() {
       return Object.fromEntries(new URLSearchParams(window.location.search).entries());
     }
   }
-  
+  /**
+   * loads components lazilly 
+   * @param {stirng} path -path to component to be lazy loaded 
+   * ###in setRoutes 
+     * ```
+     * 
+     * {
+     *  "/home":{
+     *     getComponent:lazy('./home.js'),
+     *   onLoad(){
+     *      console.log('home')
+     *  },
+     *  layout(component){
+     *    return someLayout(component)
+     *  }
+     * }
+     * 
+     * }
+     * ```
+   * @returns {this}
+   */
   export function lazy(path){
     return function(router){
       return import(path).then((s)=>s.default(router))
